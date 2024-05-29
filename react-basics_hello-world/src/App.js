8a938a8ff068748269df371b4b410cac6817c61e
Susan Rahmawati <susanrahma98@gmail.com>
2024-05-29 12:33:41 +0200

    complete challenge

--- react-basics_hello-world/src/App.js
+++ react-basics_hello-world/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Say hello...</div>;
+  return <HelloWorld />;
 }
 
 export function HelloWorld() {
