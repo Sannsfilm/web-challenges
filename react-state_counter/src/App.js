b11d9faa5da44fbab4ee67c7c518c724928c13c6
Susan Rahmawati <susanrahma98@gmail.com>
2024-06-04 11:26:03 +0200

    complete challenge

---
+++ react-state_counter/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  // let count = 0;
+  const [count, setCount] = useState(0);
+
+  function handlePuls() {
+    setCount(count + 1);
+  }
+
+  function handleMinus() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <div className="button-container">
+        <button type="button" onClick={handleMinus}>
+          -
+        </button>
+        <button type="button" onClick={handlePuls}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
