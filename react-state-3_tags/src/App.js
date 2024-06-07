1ded149ac466232f46053f32cc54927d159dba60
Susan Rahmawati <susanrahma98@gmail.com>
2024-06-07 12:25:10 +0200

    complete challenge

---
+++ react-state-3_tags/src/App.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./styles.css";
+import Form from "./components/Form/index.js";
+import List from "./components/List/index.js";
+
+export default function App() {
+  const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
+
+  function handleAddTag(newTag) {
+    setTags((tags) => [...tags, newTag]);
+  }
+
+  function handleDeleteTag(tagToDelete) {
+    setTags((tags) => tags.filter((tag) => tag !== tagToDelete));
+  }
+
+  // function handleAddTag(newTag) {
+  //   setTags([{ id: uid(), ...newTag }, ...Tag]);
+  // } ??
+
+  // function handleDeleteTag(id) {
+  //   const tagToKeep = tag.filter((tag) => {
+  //     return tag.id !== id;
+  //   });
+
+  return (
+    <main className="app">
+      <Form handleAddTag={handleAddTag} />
+      <List handleDeleteTag={handleDeleteTag} tags={tags} />
+    </main>
+  );
+}
