f5ef1da2e54cbd58e7b01576b59dbcd6ebe04556
Susan Rahmawati <susanrahma98@gmail.com>
2024-06-07 12:30:36 +0200

    add explane

--- react-state-3_tags/src/App.js
+++ react-state-3_tags/src/App.js
@@ -10,9 +10,19 @@ export default function App() {
     setTags((tags) => [...tags, newTag]);
   }
 
+  //   Fungsi ini menerima newTag sebagai argumen.
+  // Menggunakan fungsi pembaruan state setTags dengan callback (tags) => [...tags, newTag].
+  // Callback ini mengambil array tags yang ada, lalu mengembalikan array baru yang berisi semua tag yang ada plus newTag.
+  // Dengan kata lain, fungsi ini menambahkan newTag ke akhir array tags.
+
   function handleDeleteTag(tagToDelete) {
     setTags((tags) => tags.filter((tag) => tag !== tagToDelete));
   }
+
+  //   Fungsi ini menerima tagToDelete sebagai argumen.
+  // Menggunakan setTags dengan callback tags.filter((tag) => tag !== tagToDelete).
+  // Callback filter membuat array baru yang hanya berisi tag yang tidak sama dengan tagToDelete.
+  // Dengan kata lain, fungsi ini menghapus tagToDelete dari array tags.
 
   // function handleAddTag(newTag) {
   //   setTags([{ id: uid(), ...newTag }, ...Tags]);
